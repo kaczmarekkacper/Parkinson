a678557947d8620c488bbdae1b7d5199126197a2
kaczmarekkacper <kaaczmarekkacper@gmail.com>
2021-10-15 20:03:29 +0200

    Extract S1D-LBP

--- main_features.py
+++
@@ -1,9 +0,0 @@
-from ImportData import ImportData
-from tsfresh import extract_features
-
-
-if __name__ == '__main__':
-    patients = ImportData.ImportData.import_data_pandas()
-
-    df_features = extract_features(patients, column_id='ID', column_sort="Time")
-    print(df_features)
